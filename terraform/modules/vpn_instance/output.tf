432fbf3806387ec89374ca6712f4a26f094facb4
andrewduke51 <andrew.figaroa@snagajob.com>
2017-12-25 11:15:12 -0500

    remove modules

--- terraform/modules/vpn_instance/output.tf
+++
@@ -1,3 +0,0 @@
-output "public_address" {
-  value = "${aws_instance.vpn_instance.public_ip}"
-}
--- terraform/modules/vpn_instance/provider.tf
+++
@@ -1,7 +0,0 @@
-# provider
-
-provider "aws" {
-  profile                 = "${var.profile}"
-  shared_credentials_file = "${var.cred_path}"
-  region                  = "${var.default_region}"
-}
